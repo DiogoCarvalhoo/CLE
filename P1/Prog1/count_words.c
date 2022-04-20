613467037121612eaf94f2a12ee4f6bb68339347
Rafael Baptista <rafaelbaptista@ua.pt>
2022-04-20 22:49:54 +0100

    Updated time measure to CLOCK_MONOTONIC_RAW

--- P1/Prog1/count_words.c
+++ P1/Prog1/count_words.c
@@ -66,7 +66,7 @@ int main(int argc, char *argv[])
 
     struct timespec start, finish;
     double elapsed;
-    clock_gettime(CLOCK_MONOTONIC, &start);
+    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
 
     /* generate worker threads */
 
@@ -191,7 +191,7 @@ int main(int argc, char *argv[])
 
     /* measure time */
 
-    clock_gettime(CLOCK_MONOTONIC, &finish);
+    clock_gettime(CLOCK_MONOTONIC_RAW, &finish);
     elapsed = (finish.tv_sec - start.tv_sec);
     elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
 
